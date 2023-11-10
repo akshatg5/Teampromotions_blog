05f2751be2d371744095f438f439675664f25cac
Akshat Girdhar <akshatgirdhar5@gmail.com>
2023-11-10 23:28:40 +0530

    edit2

--- teampromotions/settings.py
+++ teampromotions/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = ['127.0.0.1','localhost']
 
 INSTALLED_APPS = [
     'blog',
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
